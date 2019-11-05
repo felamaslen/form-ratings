ea29b5a5e55de0db616f9979136ba272b903839f
Fela Maslen <felamaslen@gmail.com>
2019-11-05 13:13:14 +0000

    Updates

---
+++ precache-manifest.bb0373a69f674aa1df0b1637cdec2b88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2489f22be8534c01d8cd1e2af8c82625",
+    "url": "/form-ratings/index.html"
+  },
+  {
+    "revision": "90ac4413baf95e1c331f",
+    "url": "/form-ratings/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "69e24f5739981ab5ed9b",
+    "url": "/form-ratings/static/js/2.3e50480d.chunk.js"
+  },
+  {
+    "revision": "90ac4413baf95e1c331f",
+    "url": "/form-ratings/static/js/main.9bfe5da0.chunk.js"
+  },
+  {
+    "revision": "d33a7c5b3732c165a884",
+    "url": "/form-ratings/static/js/runtime-main.010d0c38.js"
+  }
+]);
