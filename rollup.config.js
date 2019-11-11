333cb085072e1008fadd65355e0fca2a6eb877df
Fela Maslen <felamaslen@gmail.com>
2019-11-11 14:21:28 +0000

    Typescript2 rollup plugin for compiler errors

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
@@ -18,25 +18,30 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
+      exports: 'named',
     },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true
+      sourcemap: true,
     },
   ],
   plugins: [
     url(),
     svgr(),
-    typescript(),
+    typescript({
+      tsconfig: 'tsconfig.json',
+      objectHashIgnoreUnknownHack: true,
+      clean: process.env.TS_ROLLUP_CLEAN === 'true',
+    }),
     babel(),
     resolve({
       preferBuiltins: true,
     }),
     commonjs({
       include: 'node_modules/**',
-    })
+    }),
   ],
   external: (id) => (
     external.includes(id)
